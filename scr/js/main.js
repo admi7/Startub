12d372a075515cf9c8a67a00755f1153bf761be5
admi7 <admi7.devops@gmail.com>
2021-05-29 15:41:45 +0000

    reorganisation des docs

--- scr/js/main.js
+++
@@ -1,4 +0,0 @@
-
-import { user } from "./script.js";
-// import { app } from "./app.js";
-import { app } from "./vue.js";
--- scr/js/script.js
+++
@@ -1,4 +0,0 @@
-export var user = {
-     prenom: 'amet',
-     nom: 'diallo'
-}
