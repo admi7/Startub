43bcd648494b7c6f8909aa64724ce7a6375e4b6c
admi7 <admi7.devops@gmail.com>
2021-05-31 10:30:16 +0000

    au travail

--- project-web/portfolio-officiel/public/src/js/app/app.js
+++ project-web/portfolio-officiel/public/src/js/app/app.js
@@ -1,8 +1,22 @@
 
 
 function sys() {
-
-
+    var tabs = document.querySelectorAll('li a')
+    for (var i=0; i>tabs.length; i++){
+        var tab = tabs[i]
+        tab.addEventListener('click', function (e) {
+            var li = this.parentNode
+            var div = this.parentNode.parentNode.parentNode
+            console.log (div)
+            if (li.classList.contains('active')){
+                return false
+            }
+    
+            div.querySelector('.tabs .active')
+            li.classList.add(active)
+    
+        })
+    }
 }
 
 
--- project-web/portfolio-officiel/public/src/js/main.module.js
+++ project-web/portfolio-officiel/public/src/js/main.module.js
@@ -1,52 +1,35 @@
 
-import { colors } from "./jquery/style.js";
-console.log(colors)
-
-
-//              ********************    LIBRERIE INTEGRE DANS LE SITE    ************************
-
-import {cookie} from "../../log/cookie.js";
-
-cookie()
-
+//              ********************    IMPORT DE MES FICHIERS    ************************
 
 import {sys} from "./app/app.js";
+import {cookie} from "../../log/cookie.js";
+import { sys } from "./app/app.js";
+
+
+
+
+//              ********************    IMPORT DE MES FICHIERS    ************************
+// ****************************************************************************************************
+//              ********************    MES SCRIPTS     ************************
+
+
+
+//              ********************    MES SCRIPTS     ************************
+// ****************************************************************************************************
+//              ********************    EXPORT DE MES FICHIERS    ************************
+
+cookie()
 sys()
 
 
-    
-var tabs = document.querySelectorAll('li a')
-for (var i=0; i>tabs.length; i++){
-    tabs[i].addEventListener('click', function (e) {
-        var li = this.parentNode
-        var div = this.parentNode.parentNode.parentNode
-
-        if (li.classList.contains('active')){
-            return false
-        }
-
-        div.querySelector('.tabs .active')
-        li.classList.add(active)
-
-    })
-}
 
 
-//              ********************    PORTE ENTRER    ************************
+//              ********************    EXPORT DE MES FICHIERS    ************************
 
-import { saisi } from './in-out/in.js'
 
-saisi('hello')
 
-//              ********************    PORTE SORTIE    ************************
 
-import {logUser} from './in-out/out.js'
 
-logUser()
-
-//              ********************    FIN    ************************
-
-//              ********************    TEST DE MES FUNCTIONS    ************************
 
 
 
